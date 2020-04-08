7215d826093a1d0d690f6d3733c558c315d2a394
Rodolfo L <rodolfo.nat@gmail.com>
2020-04-08 20:12:32 -0300

    hacker rank tests

---
+++ hackerRank/countApplesAndOranges/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        // Complete the countApplesAndOranges function below.
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            // a: apple tree
+            int totalApple = 0;
+            for (int i = 0; i < apples.Length; i++)
+            {
+                int distance = a + apples[i];
+                if (distance >= s && distance <= t)
+                    totalApple++;
+            }
+
+            // b: orange tree
+            int totalOranges = 0;
+            for (int i = 0; i < oranges.Length; i++)
+            {
+                int distance = b + oranges[i];
+                if (distance >= s && distance <= t)
+                    totalOranges++;
+            }
+
+            Console.WriteLine(totalApple);
+            Console.WriteLine(totalOranges);
+        }
+        static void Main(string[] args)
+        {
+            string[] st = "7 11".Split(' ');
+
+            int s = Convert.ToInt32(st[0]);
+
+            int t = Convert.ToInt32(st[1]);
+
+            string[] ab = "5 15".Split(' ');
+
+            int a = Convert.ToInt32(ab[0]);
+
+            int b = Convert.ToInt32(ab[1]);
+
+            string[] mn = "3 2".Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+
+            int[] apples = Array.ConvertAll("-2 2 1".Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+            ;
+
+            int[] oranges = Array.ConvertAll("5 -6".Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+            ;
+            countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        }
+    }
+}
---
+++ hackerRank/selectionSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace selectionSort
+{
+    class Program
+    {
+        static int[] Sort(int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = i + 1; j < list.Length; j++)
+                {
+                    if (list[j] < list[i])
+                    {
+                        int aux = list[i];
+                        list[i] = list[j];
+                        list[j] = aux;
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        static void Print(int[] list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintAndSort(string message, int[] list)
+        {
+            Console.WriteLine(message);
+
+            Print(list);
+            list = Sort(list);
+            Print(list);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] list;
+
+            // random
+            list = new int[] { 8, 4, 1, 8, 9, 3 };
+            PrintAndSort("random", list);
+
+            // sorted 
+            list = new int[] { 1, 1, 5, 8, 10, 50, 50 };
+            PrintAndSort("sorted", list);
+
+            // inverted
+            list = new int[] { 50, 40, 30, 20, 10, 1 };
+            PrintAndSort("inverted", list);
+        }
+    }
+}
